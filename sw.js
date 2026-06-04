522c2ebacd955eb68dd272779ef308ca1431a536
Elliot 🦖 <elliot@MacBook-Air-de-Elliot.local>
2026-06-04 04:18:29 +0200

    Suppression avec confirmation prénom, accordéon settings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pointage-v4';
+const CACHE = 'pointage-v5';
 const FILES = ['.', 'index.html', 'style.css', 'data.js', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
